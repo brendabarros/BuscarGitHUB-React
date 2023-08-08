959c3fe498a595ea74c52d5cc253e904218e5aea
marambyte <85901562+brendabarros@users.noreply.github.com>
2023-08-07 21:44:27 -0300

    Componente erro adicionado
    Componente erro adicionado

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import Search from '../components/Search'
 import { UserProps } from '../types/user';
+import  User from '../components/User'
+import Error from '../components/Error';
 
 const Home = () => {
   const [user,setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
  
   const loadUser= async (userName: string)=>{
+
+    setError(false);
+    setUser(null)
 
     const res= await fetch (`https://api.github.com/users/${userName}`)
 
     const data= await res.json();
+    if (res.status === 404) {
+      setError(true)
+      return;
+    }
 
     const {avatar_url, login, location, followers, following} = data
 
@@ -25,7 +35,8 @@ const Home = () => {
 
   return (
     <div><Search loadUser ={loadUser}/>
-    {user &&<p>{user.login}</p>}
+    {user && <User {...user}/>}
+    {error && <Error /> }
     </div>
   )
 }
