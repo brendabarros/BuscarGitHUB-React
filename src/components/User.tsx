77a939713b5ae4b0b82193b3bff52b9b01e72459
marambyte <85901562+brendabarros@users.noreply.github.com>
2023-08-08 14:40:19 -0300

    estilização final
    finalização do projeto da video aula

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,34 +1,37 @@
 import { UserProps } from "../types/user";
-import {MdLocationPin} from 'react-icons/md'
+import { MdLocationPin } from 'react-icons/md'
 import { Link } from "react-router-dom";
+import classes from "./User.module.css"
 
 const User = ({
     login,
-    avatar_url, 
+    avatar_url,
     followers,
-     following,
-      location,
-     }: UserProps) => {
+    following,
+    location,
+}: UserProps) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
-                <p>
-                    <MdLocationPin/>
+                <p className={classes.location}>
+                    <MdLocationPin />
                     <span>{location}</span>
                 </p>
             )}
-            <div>
-                <p>Seguidores:</p>
-                <p>{followers}</p>
-            </div>
-            <div>
-                <p>Seguindo:</p>
-                <p>{following}</p>
+            <div className={classes.stats}>
+                <div>
+                    <p>Seguidores:</p>
+                    <p className={classes.number}>{followers}</p>
+                </div>
+                <div>
+                    <p >Seguindo:</p>
+                    <p className={classes.number}>{following}</p>
+                </div>
             </div>
             <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
-        
+
         </div>
     )
 }
